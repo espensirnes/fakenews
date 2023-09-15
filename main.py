a39976d1c36eddc1ef14f5964fea04f9595a7854
espensirnes <espen.sirnes@uit.no>
2023-09-15 14:42:54 +0200

    afd

--- main.py
+++ main.py
@@ -8,19 +8,8 @@ pt.options.pqdkm.set([1,1,0,2,2])
 pt.options.fixed_random_group_eff.set(0)
 pt.options.fixed_random_time_eff.set(0)
 pt.options.EGARCH.set(False)
+pt.options.ARMA_round.set(16)
 
-
-print("REAL NEWS ************************************")
-df = pd.read_excel("RealNewsDjiaReturns.xlsx")
-df = df.rename(columns={'Real News':'Real_news'})
-
-
-df['LRealNews1'] = df['Real_news'].shift(1)
-df = df.dropna()
-
-s = pt.execute("return~LRealNews1",df,T='Date')
-print(s)
-print("****************************\n\n\n\n\n")
 
 
 
@@ -33,7 +22,27 @@ df['LNews_sentiment1'] = df['News_sentiment'].shift(1)
 df = df.dropna()
 
 s = pt.execute("return~LNews_sentiment1",df,T='Date')
+
 print(s)
+
+print("****************************\n\n\n\n\n")
+
+print("REAL NEWS ************************************")
+df = pd.read_excel("RealNewsDjiaReturns.xlsx")
+df = df.rename(columns={'Real News':'Real_news'})
+
+
+df['LRealNews1'] = df['Real_news'].shift(1)
+df = df.dropna()
+
+s = pt.execute("return~LRealNews1",df,T='Date')
+print(s)
+
+
+
+
+
+
 
 
 
